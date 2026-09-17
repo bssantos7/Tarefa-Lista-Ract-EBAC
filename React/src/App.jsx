2d74acd17cfdd92b5136a4fc5ec540ff6093069d
bs.santos7 <bs.santos7@hotmail.com>
2026-09-17 01:03:39 -0300

    feature  menu funcionando

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,28 +1,40 @@
 import { useState } from 'react'
 import {useContext} from 'react'
 import {UsuarioContext} from './Context/UsuarioContext'
+import PaginaContext from './Context/PaginaContext'
 import TelaLogin from './Componentes/TelaLogin'
-
 import Menu from './Componentes/Menu'
+import TelaCadastro from './Componentes/TelaCadastro'
+import TelaGestao from './Componentes/TelaGestao'
+
 
 
 function App() {
-  
+  const{paginaAtual, setPaginaAtual} = useContext(PaginaContext);
+
+  function renderizarTela(){
+    switch(paginaAtual){
+      case 'login':
+        return <TelaLogin />
+      case 'cadastro':
+        return <TelaCadastro />
+      case 'gestao':
+        return <TelaGestao />
+      default:
+        return <TelaLogin />
+    }
+  }
 
   return (
     <>
       <div className="container-md">
         <div className="row">
           <div class="col">
-
             <Menu />
           </div>
         </div>
         <div className="row">
-          <div class="col">
-            <p>Bem-vindo ao sistema de gerenciamento de tarefas. Faça login para acessar suas tarefas.</p>
-            <TelaLogin />
-          </div>
+          {renderizarTela()}
         </div>  
       </div>
     </>
---
+++ React/src/Componentes/TelaCadastro.jsx
@@ -0,0 +1,8 @@
+function TelaCadastro() {
+    return (
+        <div>
+            <h1>Tela de Cadastro</h1>
+        </div>
+    )
+}
+export default TelaCadastro;
